6512808a2ca6bd70b3af8adc263662673db768aa
mateusamarall <mateus.amaral018@gmail.com>
2020-07-22 20:19:03 -0300

    finalizando CRUD de usuários

--- KnexJS/src/Controllers/UserController.js
+++ KnexJS/src/Controllers/UserController.js
@@ -5,4 +5,41 @@ module.exports = {
 
     return res.json(results);
   },
+
+  async store(req, res, next) {
+    try {
+      const { username } = req.body;
+      await knex("users").insert({
+        username,
+      });
+
+      return res.json({ message: "usuário criado com sucesso" });
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  async update(req, res, next) {
+    try {
+      const { username } = req.body;
+      const { id } = req.params;
+
+      await knex("users").update({ username }).where({ id });
+
+      return res.json({ id, username });
+    } catch (error) {
+      next(error);
+    }
+  },
+  async delete(req, res, next) {
+    try {
+      const { id } = req.params;
+
+      await knex("users").where({ id }).del();
+
+      return res.json({ message: "usuário deletado com sucesso" });
+    } catch (error) {
+      next(error);
+    }
+  },
 };
--- KnexJS/src/routes.js
+++ KnexJS/src/routes.js
@@ -4,5 +4,8 @@ const UserController = require("./Controllers/UserController");
 const routes = express.Router();
 
 routes.get("/users", UserController.index);
+routes.post("/users", UserController.store);
+routes.put("/users/:id", UserController.update);
+routes.delete("/users/:id", UserController.delete);
 
 module.exports = routes;
