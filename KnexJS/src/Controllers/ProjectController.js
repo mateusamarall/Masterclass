26a5b90d2a3884e346a17d3a9751873df1965c1b
mateusamarall <mateus.amaral018@gmail.com>
2020-07-22 22:26:15 -0300

    paginação de projects

---
+++ KnexJS/src/Controllers/ProjectController.js
@@ -0,0 +1,45 @@
+const knex = require("../database");
+module.exports = {
+  async index(req, res, next) {
+    try {
+      const { user_id, page = 1 } = req.query;
+      const query = knex("projects")
+        .limit(5)
+        .offset((page - 1) * 5);
+
+      const countObj = knex("projects").count();
+
+      if (user_id) {
+        query
+          .where({ user_id })
+          .join("users", "user_id", "=", "projects.user_id")
+          .select("projects.*", "users.username");
+
+        countObj.where({ user_id });
+      }
+
+      const [count] = await countObj;
+
+      res.header("X-Total-Count", count["count"]);
+
+      const results = await query;
+
+      return res.json(results);
+    } catch (error) {
+      next(error);
+    }
+  },
+  async store(req, res, next) {
+    try {
+      const { title, user_id } = req.body;
+      await knex("projects").insert({
+        title,
+        user_id,
+      });
+
+      return res.json({ message: "Projeto criado com sucesso" });
+    } catch (error) {
+      next(error);
+    }
+  },
+};
--- KnexJS/src/routes.js
+++ KnexJS/src/routes.js
@@ -1,11 +1,16 @@
 const express = require("express");
 
 const UserController = require("./Controllers/UserController");
+const ProjectController = require("./Controllers/ProjectController");
+
 const routes = express.Router();
 
 routes.get("/users", UserController.index);
 routes.post("/users", UserController.store);
 routes.put("/users/:id", UserController.update);
 routes.delete("/users/:id", UserController.delete);
+
+routes.get("/projects", ProjectController.index);
+routes.post("/projects", ProjectController.store);
 
 module.exports = routes;
