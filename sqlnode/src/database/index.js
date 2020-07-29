16199a611921a8e114699daef4a47e46b8ef122d
mateusamarall <mateus.amaral018@gmail.com>
2020-07-29 20:50:07 -0300

    adicionado assosiação de address e users

--- sqlnode/src/database/index.js
+++ sqlnode/src/database/index.js
@@ -1,8 +1,13 @@
 const Sequelize = require("sequelize");
 const dbconfig = require("../config/database");
 const User = require("../models/User");
+const Address = require("../models/Address");
+
 const connection = new Sequelize(dbconfig);
 
 User.init(connection);
+Address.init(connection);
+
+Address.associate(connection.models);
 
 module.exports = connection;
