ac7ff73ff4369ee555e98486bec1c66690644d3d
mateusamarall <mateus.amaral018@gmail.com>
2020-07-29 19:46:08 -0300

    importado model de usuário

--- sqlnode/src/database/index.js
+++ sqlnode/src/database/index.js
@@ -1,6 +1,8 @@
 const Sequelize = require("sequelize");
 const dbconfig = require("../config/database");
-
+const User = require("../models/User");
 const connection = new Sequelize(dbconfig);
+
+User.init(connection);
 
 module.exports = connection;
