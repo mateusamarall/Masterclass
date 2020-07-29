52c9c4327f5dde8124f7151cff1146aaae0f3656
mateusamarall <mateus.amaral018@gmail.com>
2020-07-29 20:51:02 -0300

    adicionado rota para criação de address

--- sqlnode/src/routes.js
+++ sqlnode/src/routes.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const UserController = require("./Controllers/UserController");
+const AddressController = require("./Controllers/AddressController");
+
 const routes = express.Router();
 
 routes.post("/users", UserController.store);
+routes.get("/users", UserController.index);
+
+routes.post("/users/:user_id/addresses", AddressController.store);
 
 module.exports = routes;
