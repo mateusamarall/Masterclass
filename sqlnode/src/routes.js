5738a9f20d24b035ac28f3f4fd031c68348b1a87
mateusamarall <mateus.amaral018@gmail.com>
2020-07-31 09:14:39 -0300

    rotas para tecnologias

--- sqlnode/src/routes.js
+++ sqlnode/src/routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const UserController = require("./Controllers/UserController");
 const AddressController = require("./Controllers/AddressController");
+const TechController = require("./Controllers/TechController");
 
 const routes = express.Router();
 
@@ -9,5 +10,9 @@ routes.get("/users", UserController.index);
 
 routes.post("/users/:user_id/addresses", AddressController.store);
 routes.get("/users/:user_id/addresses", AddressController.index);
+
+routes.get("/users/:user_id/techs", TechController.index);
+routes.post("/users/:user_id/techs", TechController.store);
+routes.delete("/users/:user_id/techs", TechController.delete);
 
 module.exports = routes;
