d038e5cf3d03cdd0ac4e7336ed0335682932c8f6
mateusamarall <mateus.amaral018@gmail.com>
2020-07-30 21:50:44 -0300

    rota para listagem de endereço

--- sqlnode/src/routes.js
+++ sqlnode/src/routes.js
@@ -8,5 +8,6 @@ routes.post("/users", UserController.store);
 routes.get("/users", UserController.index);
 
 routes.post("/users/:user_id/addresses", AddressController.store);
+routes.get("/users/:user_id/addresses", AddressController.index);
 
 module.exports = routes;
